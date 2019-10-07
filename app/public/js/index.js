2e513c042459d7a81d43dd99a8528f1aaa725fe3
calemars <54514644+calemars@users.noreply.github.com>
2019-10-06 20:38:09 -0400

    initial html setup
    setup pages and folder structure for d&s project

---
+++ app/public/js/index.js
@@ -0,0 +1,17 @@
+function addRegistration() {
+  var x = document.getElementById("registration");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function addCertification() {
+  var x = document.getElementById("certification");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
